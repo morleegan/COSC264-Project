b269600f4bb08f58ceddf1018f3005a2b1677c9b
Morgan <mpeter15@hawk.iit.edu>
2016-09-02 00:18:49 +1200

    sender worked on. Some changes

--- udp_socket.py
+++ udp_socket.py
@@ -17,46 +17,46 @@ class Packet:
     """The Packet Type"""
     
     def __init__(self, magicno, ptype, seqno, dataLen, data=None):
-	# magicno: 0x497E if not print a failure message drop packet imediately
-	self.magicno = magicno
-	# ptype: dataPacket (0) or acknowledgementPacket (1)
-	self.ptype = ptype
-	# seqno: bit value 0 or 1
-	self.seqno = seqno
-	# dataLen: 0-512, num of user bytes
-	self.dataLen = dataLen
-	# data: depends on dataLength and contains the user data
-	if data is None:
-	    self.data = []
-	else:
-	    self.data.append(data) # appending data
-    
-    def check_ack_packet(self):
-	"""Checks the packet is an ack packet with length of ack packet"""
-	if self.ptype == PTYPE_ACK:
-	    if self.dataLen != 0:
-		# drop packet
-		print("Corrupt Packet")
-		return False
-		# raise Exception("Corrupt Packet")
-	    else:
-		return True
+        # magicno: 0x497E if not print a failure message drop packet imediately
+        self.magicno = magicno
+        # ptype: dataPacket (0) or acknowledgementPacket (1)
+        self.ptype = ptype
+        # seqno: bit value 0 or 1
+        self.seqno = seqno
+        # dataLen: 0-512, num of user bytes
+        self.dataLen = dataLen
+        # data: depends on dataLength and contains the user data
+        if data is None:
+            self.data = []
+        else:
+            self.data.append(data) # appending data
+
+        def check_ack_packet(self):
+        """Checks the packet is an ack packet with length of ack packet"""
+        if self.ptype == PTYPE_ACK:
+            if self.dataLen != 0:
+            # drop packet
+            print("Corrupt Packet")
+            return False
+            # raise Exception("Corrupt Packet")
+            else:
+            return True
 	
     def check_end_data_packet(self):
-	"""Checks if end of transmission"""
-	if self.ptype == dataPacket and self.dataLen == 0:
-	    # end of transmitted file
-	    return True
-	else:
-	    return False
-    
+        """Checks if end of transmission"""
+        if self.ptype == dataPacket and self.dataLen == 0:
+            # end of transmitted file
+            return True
+        else:
+            return False
+
     def check_magicno(self):
-	"""Checks self.magicno = 0x497E else drops the packet"""
-	if self.magicno != MAGICNO:
-	    # drop packet imediately
-	    return False
-	else:
-	    return True
+        """Checks self.magicno = 0x497E else drops the packet"""
+        if self.magicno != MAGICNO:
+            # drop packet imediately
+            return False
+        else:
+            return True
 
 
 def sender(port_sin, port_sout, port_csin, file_name):

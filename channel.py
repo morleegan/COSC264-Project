2fe5c0f4c00a02c6a05c7362744180d6e938835c
Morgan <mpeter15@hawk.iit.edu>
2016-09-08 23:34:27 +1200

    Channel updated... Just one more before it might work >_<

--- channel.py
+++ channel.py
@@ -2,7 +2,7 @@
 import socket  # for sockets
 import select  # for listening nicely on sockets
 import random  # for uniform
-from packet_class.py import *
+from packet_class import *
 
 PTYPE_DATA = 0
 PTYPE_ACK = 1
@@ -25,61 +25,66 @@ class Channel:
         self.sin = sin
         self.rin = rin
         self.p_rate = p_rate
+        self.socket_sin = self.create_socket(self.c_sin)
+        self.socket_sout = self.create_socket(self.c_sout)
+        self.socket_rin = self.create_socket(self.c_rin)
+        self.socket_rout = self.create_socket(self.c_rout)
+        self.check_port()
 
-    def channel(self):
+    def create_socket(self, port):
+        #  socket creation
+        new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        new_socket.bind((IP, port))
+        return new_socket
 
-        if not all(1024 <= x <= 64000 and isinstance(x, int) == True \
-                   for x in (self.c_sin, self.c_sout, self.c_rin, self.c_rout)):
-            raise ValueError("Not correct port type")
+    def connect_socket(self):
+        self.socket_sout.connect((IP, self.sin))
+        self.socket_rout.connect((IP, self.rin))
 
-        if not (0 <= self.p_rate < 1):
-            raise ValueError("Not correct probability value")
-        c_sin_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        c_sin_sock.bind((IP, self.c_sin))
-
-        c_sout_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        c_sout_sock.bind((IP, self.c_sout))
-
-        c_rin_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        c_rin_sock.bind((IP, self.c_rin))
-
-        c_rout_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        c_rout_sock.bind((IP, self.c_rout))
-
-        c_sout_sock.connect((IP, self.sin))
-        c_rout_sock.connect((IP, self.rin))
-
-        # enter infinite loop
+    def send_to(self):
+        self.connect_socket()
         while True:
-            # use select in blocking fashion to save CPU time
-            read_sock, write_sock, error_sock = select.select([self.c_sin,
-                                                               self.c_rin],
-                                                              [], [])
-            for sock in read_sock:  # received packet on socket
-                rcvd = sock.recv(MAX_READ_SIZE)
+            socket_read, socket_write, error_socket = select.select(
+                    [self.c_sin, self.c_rin], [], [])
+
+            for sock in socket_read:
                 if not sock.check_magicno():
-                    continue  # stop processing and return to start of loop
+                    #  if the socket does not have MAGICNO correct get next
+                    continue
+                received = sock.recv(MAX_READ_SIZE)
+                random_var = random.uniform(0, 1)
+                if random_var < self.p_rate:
+                    continue
+                elif sock == self.socket_sin:
+                    self.send_packet(self.socket_sin, received)
+                elif sock == self.socket_rin:
+                    self.send_packet(self.socket_rin, received)
+                elif error_socket == TIMEOUT:
+                    self.socket_close()
 
-                u = random().uniform()  # uniformly distributed random variate
-                if u < self.p_rate:
-                    continue  # stop processing and return to start of loop
-                elif sock == self.c_sin:  # packet received on c_sin_sock:
-                    try:
-                        c_rout_sock.send(rcvd)  # packet is sent on crout to rin
-                    except:
-                        print("Connection Lost")
-                        return
-                elif sock == self.c_rin:  # packet received on c_rin_sock:
-                    try:
-                        c_sout_sock.send(rcvd)  # packet is sent on csout to sin
-                    except:
-                        print("Connection Lost")
-                        return
+    def send_packet(self, socket1, received):
+        try:
+            # packet is sent on crout to rin
+            socket1.send(received)
+        except:
+            print("Connection Failed")
 
-        c_sin_sock.close()
-        c_sout_sock.close()
-        c_rin_sock.close()
-        c_rout_sock.close()
+    def check_p_rate(self):
+        #  check p rate
+        if not (0 <= self.p_rate < 1):
+            exit(-1)
+
+    def check_port(self):
+        #  check port numbers
+        if not all(1024 <= x <= 64000 and isinstance(x, int) is True
+                   for x in (self.c_sin, self.c_sout, self.c_rin, self.c_rout)):
+            exit(-1)
+
+    def socket_close(self):
+        self.socket_rin.close()
+        self.socket_rout.close()
+        self.socket_sin.close()
+        self.socket_sout.close()
 
 
 
